a62a86e7303eeae6871a03e6a1828c3c88e4cdfc
Anand Narayan <arnarayan@gmail.com>
2020-09-20 12:21:30 -0600

    creating the list of tools component

---
+++ my-gh-angular/src/app/list-of-tools/list-of-tools.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-list-of-tools',
+  templateUrl: './list-of-tools.component.html',
+  styleUrls: ['./list-of-tools.component.css']
+})
+export class ListOfToolsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
